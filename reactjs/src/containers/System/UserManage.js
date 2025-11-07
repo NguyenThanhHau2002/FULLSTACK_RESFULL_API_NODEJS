41bdaba1a862992fe67e758ccebce48c549920d1
NguyenThanhHau2002 <99946413+NguyenThanhHau2002@users.noreply.github.com>
2025-11-07 23:36:14 +0700

    11:35PM 11/7/2025

--- reactjs/src/containers/System/UserManage.js
+++ reactjs/src/containers/System/UserManage.js
@@ -1,83 +1,112 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-import './UserManage.scss';
-import { getAllUsers } from '../../services/userService';
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
+import ModalUser from "./ModalUser";
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+      isOpenModalUser: false,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
     }
+  }
+  /*** life cycle
+   *  Run component:
+   * 1. Run construct -> init state
+   * 2. Did mount (set state) : born ; unmount
+   * 3. Render(re- render)
+   */
 
-    async componentDidMount() {
-        let response = await getAllUsers('ALL');
-        if(response && response.errCode ===0) {
-            this.setState({
-                arrUsers: response.users
-            })
-        }
-    }
-    /*** life cycle
-     *  Run component:
-     * 1. Run construct -> init state
-     * 2. Did mount
-     * 3. Render
-     */
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+  };
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
 
-    render() {
-        let arrUsers = this.state.arrUsers;
-        return (
-            <div className="users-container">
-                <div className='title text-center'> Manage users with Nguyen Thanh Hau</div>
-                <div className='users-table mt-3 mx-1'>
-                <table id="customers">
-                    <tr>
-                        <th>Email</th>
-                        <th>Fist name</th>
-                        <th>Last name</th>
-                        <th>Address</th>
-                        <th>Role</th>
-                        <th>Phone number</th>
-                        <th>Action</th>
-                    </tr>
-                        {
-                            arrUsers && arrUsers.map((item, index) => {
-                                console.log('eric check map', item, index)
-                                return(  
-                                    <tr key={index}>
-                                        <td>{item.email}</td>
-                                        <td>{item.firstName}</td>
-                                        <td>{item.lastName}</td>
-                                        <td>{item.address}</td>
-                                        <td>{item.roleId}</td>
-                                        <td>{item.phonenumber}</td>
-                                        <td>
-                                            <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
-                                            <button className='btn-delete'><i className="fas fa-trash"></i></button>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </table>
-                </div>
-            </div>
-        );
-    }
-
+  render() {
+    let arrUsers = this.state.arrUsers;
+    //console.log(arrUsers);
+    //properties ; nested
+    return (
+      <div className="users-container">
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          toggleFromParent={this.toggleUserModal}
+          test={"abc"}
+        />
+        <div className="title text-center">
+          Manage users with Nguyen Thanh Hau
+        </div>
+        <div className="mx-1">
+          <button
+            className="btn btn-primary px-3"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i className="fas fa-plus"> </i>Add new users
+          </button>
+        </div>
+        <div className="users-table mt-3 mx-1">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>Fist name</th>
+              <th>Last name</th>
+              <th>Address</th>
+              <th>Role</th>
+              <th>Phone number</th>
+              <th>Action</th>
+            </tr>
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                console.log("Repben check map", item, index);
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>{item.roleId}</td>
+                    <td>{item.phonenumber}</td>
+                    <td>
+                      <button className="btn-edit">
+                        <i className="fas fa-pencil-alt"></i>
+                      </button>
+                      <button className="btn-delete">
+                        <i className="fas fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
