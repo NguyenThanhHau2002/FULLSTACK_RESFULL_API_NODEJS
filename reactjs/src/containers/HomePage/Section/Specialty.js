28eaa3ba5c1491c7a95829d355bc129ea14713a7
NguyenThanhHau2002 <99946413+NguyenThanhHau2002@users.noreply.github.com>
2025-11-25 02:19:06 +0700

    2:16AM 11/25/2025 React-Slider-Carousel and add React-Slider-Carousel

---
+++ reactjs/src/containers/HomePage/Section/Specialty.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Specialty.scss";
+import { FormattedMessage } from "react-intl";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+class Specialty extends Component {
+  render() {
+    let settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    };
+    return (
+      <div className="section-specialty">
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span className="title-section">Chuyên Khoa Phổ Biến</span>
+            <button className="btn-section">Xem Thêm</button>
+          </div>
+          <div className="specialty-body">
+            <Slider {...settings}>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>Cơ Xương Khớp 1</div>
+              </div>
+              <div className="specialty-customize">
+                <div>
+                  <div className="bg-image" />
+                  <div>Cơ Xương Khớp 2</div>
+                </div>
+              </div>
+              <div className="specialty-customize">
+                <div>
+                  <div className="bg-image" />
+                  <div>Cơ Xương Khớp 3</div>
+                </div>
+              </div>
+              <div className="specialty-customize">
+                <div>
+                  <div className="bg-image" />
+                  <div>Cơ Xương Khớp 4</div>
+                </div>
+              </div>
+              <div className="specialty-customize">
+                <div>
+                  <div className="bg-image" />
+                  <div>Cơ Xương Khớp 5</div>
+                </div>
+              </div>
+              <div className="specialty-customize">
+                <div>
+                  <div className="bg-image" />
+                  <div>Cơ Xương Khớp 6</div>
+                </div>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
