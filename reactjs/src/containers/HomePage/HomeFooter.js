79f2a6d73b89c9b674397c03880b2e2b30ee9aea
NguyenThanhHau2002 <99946413+NguyenThanhHau2002@users.noreply.github.com>
2025-11-25 23:51:00 +0700

    11:50PM 11/25/2025 Hoan Thanh Giao dien va them sticky vao header

---
+++ reactjs/src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+
+class HomeFooter extends Component {
+  render() {
+    return (
+      <div className="home-footer">
+        <p>
+          &copy; 2025 Nguyen Thanh Hau voi Repben.{" "}
+          <a target="_blank" href="#">
+            More information, please visit my. &#8594; Click here &#8592;
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
---
+++ reactjs/src/containers/HomePage/Section/About.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="section-share section-about">
+        <div className="section-about-header">
+          Truyen Thong noi ve Nguyen Thanh Hau
+        </div>
+        <div className="section-about-content">
+          <div className="content-left">
+            <iframe
+              width="100%"
+              height="400px"
+              src="https://www.youtube.com/embed/0tG0V22Hy5c"
+              title="Restream hôm qua........................."
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="content-right">
+            <p>Nguyen Thanh Hau</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
